cd88d128d4cea930d3dd51d33cef8043f6ec8749
TheLime03 <everpadd4@gmail.com>
2024-09-29 16:58:07 +0100

    init

--- TD2/src/Animal.java
+++ TD2/src/Animal.java
@@ -1,16 +1,24 @@
 public class Animal {
-    String family;
-	String name;
-	int age;
-	boolean isMammal;
+    public String family;
+    public String name;
+    public int age;
+    public boolean isMammal;
 
-    Animal(String family, String name, int age, boolean isMammal){
+    public Animal() {
+        family = "";
+        name = "";
+        age = 0;
+        isMammal = false;
+    }
+
+    public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
         this.age = age;
         this.isMammal = isMammal;
     }
-
-    @overide
+public String toString() {
+        return "Name: "+name+ ",age:"+age+" , isMammal:"+isMammal;
+}
 
 }
